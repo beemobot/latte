6cff62bf933b25b7bb35eed06260928999826ff9
Ayu <ayu@ayu.dev>
2022-01-13 15:11:57 -0800

    Add shared match & tea files

---
+++ lib/src/main/java/dev/ayu/latte/ratelimit/RatelimiterType.java
@@ -0,0 +1,38 @@
+public enum RatelimiterType {
+
+    GLOBAL,
+    IDENTIFY;
+
+    private static final String KAFKA_GLOBAL_REQUESTS_TOPIC = "discord-global-ratelimit-requests";
+    private static final String KAFKA_GLOBAL_GRANTS_TOPIC = "discord-global-ratelimit-grants";
+
+    private static final String KAFKA_IDENTIFY_REQUESTS_TOPIC = "discord-identify-ratelimit-requests";
+    private static final String KAFKA_IDENTIFY_GRANTS_TOPIC = "discord-identify-ratelimit-grants";
+
+    public String getRequestsTopic() {
+        switch (this) {
+            case GLOBAL -> {
+                return KAFKA_GLOBAL_REQUESTS_TOPIC;
+            }
+            case IDENTIFY -> {
+                return KAFKA_IDENTIFY_REQUESTS_TOPIC;
+            }
+        }
+        throw new AssertionError("Requests topic not implemented for "
+                + this + " RatelimitType");
+    }
+
+    public String getGrantsTopic() {
+        switch (this) {
+            case GLOBAL -> {
+                return KAFKA_GLOBAL_GRANTS_TOPIC;
+            }
+            case IDENTIFY -> {
+                return KAFKA_IDENTIFY_GRANTS_TOPIC;
+            }
+        }
+        throw new AssertionError("Grants topic not implemented for "
+                + this + " RatelimitType");
+    }
+
+}
