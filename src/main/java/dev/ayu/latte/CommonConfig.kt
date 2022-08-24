857125e73f386e3bc14bd09cfeded2ec40e90774
Adrian Paschkowski <git@wasdennnoch.me>
2022-08-24 17:40:38 +0200

    Move Matcha cluster ID to new file
    In the future, it'll probably be used for more than just rate limits, so it doesn't make sense to put it in the ratelimit config.

---
+++ src/main/java/dev/ayu/latte/CommonConfig.kt
@@ -0,0 +1,7 @@
+package dev.ayu.latte
+
+object CommonConfig {
+
+    const val MATCHA_CLUSTER_ID = -1
+
+}
--- src/main/java/dev/ayu/latte/ratelimit/SharedRatelimitData.kt
+++ src/main/java/dev/ayu/latte/ratelimit/SharedRatelimitData.kt
@@ -4,8 +4,6 @@ import com.squareup.moshi.JsonClass
 
 object SharedRatelimitData {
 
-    const val MATCHA_CLUSTER_ID = -1
-
     const val RATELIMIT_TOPIC = "ratelimit"
 
     const val KEY_REQUEST_IDENTIFY_QUOTA = "request-identify-quota"
